2df5da8ce802b707a33e325e3394da29a865c404
tarnishablec <tarnishablec@outlook.com>
2020-05-03 15:23:40 +0800

    notest new

--- scripts/new.js
+++ scripts/new.js
@@ -1,6 +1,10 @@
 const { run, resolveTargets } = require('./utils')
 const { scope } = require('./setting')
 const args = require('minimist')(process.argv.slice(2))
+const fse = require('fs-extra')
+const path = require('path')
+
+const cleanTest = args.notest
 
 main()
 
@@ -8,6 +12,13 @@ async function main() {
   const cache = [...args._]
   await createPackages(args._)
   await run(`yarn run boot ${resolveTargets(cache).join(' ')} --init --force`)
+  if (cleanTest) {
+    cache.forEach((c) =>
+      fse.rmdirSync(path.resolve(`packages/${c}/__tests__`), {
+        recursive: true
+      })
+    )
+  }
 }
 
 async function createPackages(names) {
